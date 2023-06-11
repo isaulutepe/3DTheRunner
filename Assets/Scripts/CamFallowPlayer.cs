b139c0e68c2aaeb048faf63b39d1da0a3a2df189
İsa <ulutepe42@gmail.com>
2023-06-11 19:15:25 +0300

    Addet playerContoller and CameraFallow Script

---
+++ Assets/Scripts/CamFallowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFallowPlayer : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float sSpeed = 10.0f;
+    public Vector3 distance;
+    public Transform lookTarget;
+
+    private void LateUpdate()
+    {
+        Vector3 distancePos = cameraTarget.position + distance; //Takip mesafesi
+        Vector3 speedPos = Vector3.Lerp(transform.position, distancePos, sSpeed * Time.deltaTime); //Takip hýzý
+        transform.position = speedPos;
+        transform.LookAt(lookTarget.position);
+    }
+}
