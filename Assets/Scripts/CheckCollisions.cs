fe3c373c106f51f4b3b37702ea46183f4a4f42de
İsa <ulutepe42@gmail.com>
2023-06-12 21:29:34 +0300

    Added Collect Coin adn Added Score UI

---
+++ Assets/Scripts/CheckCollisions.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CheckCollisions : MonoBehaviour
+{
+    //Çarpýþma kontrollerinin yapýlacaðý script dosyasýdýr.
+
+    public int score;
+    public TextMeshProUGUI CoinText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            AddCoin();
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    public void AddCoin()
+    {
+        score++;
+        CoinText.text="Score: "+score.ToString();
+
+    }
+
+}
